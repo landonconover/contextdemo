444f23d30705d6305a73515934cd81598a2bb10a
Landon Conover <landon.conover@gmail.com>
2022-03-26 15:55:53 -0600

    Basic Reducer Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useReducer, useState } from 'react';
 import './App.css';
+
+const initialState = {count: 0}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment' :
+      return {count: state.count + 1};
+    case 'decrement' :
+      return {count: state.count - 1};
+    case 'reset':
+      return {count: 0}
+    default:
+      throw new Error();
+  }
+}
+
+function Counter({initialCount}) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter initialCount={0} />
     </div>
   );
 }
